c5e1351616dd2cb5f0fe0822c8e985c13345e16c
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2024-09-13 12:54:54 +0530

    add: add tauri plugin decorum

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,8 @@
+use tauri::Manager;
+
+use tauri_plugin_decorum::WebviewWindowExt; // adds helper methods to WebviewWindow
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +12,28 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+    .plugin(tauri_plugin_decorum::init()) // initialize the decorum plugin
+		.setup(|app| {
+			// Create a custom titlebar for main window
+			// On Windows this hides decoration and creates custom window controls
+			// On macOS it needs hiddenTitle: true and titleBarStyle: overlay
+			let main_window = app.get_webview_window("main").unwrap();
+			main_window.create_overlay_titlebar().unwrap();
+
+			// Some macOS-specific helpers
+			#[cfg(target_os = "macos")] {
+				// Set a custom inset to the traffic lights
+				main_window.set_traffic_lights_inset(12.0, 16.0).unwrap();
+
+				// Make window transparent without privateApi
+				main_window.make_transparent().unwrap();
+
+				// Set window level
+				// NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
+				main_window.set_window_level(25).unwrap();
+			}
+			Ok(())
+		})
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
